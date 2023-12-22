eeb324d1120112978c3dfaafbe919d0a0b182064
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-12-22 23:43:38 +0700

    feat: update Home

--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -1,18 +1,74 @@
 const Home = () => {
+  const Posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      difficulty: "easy",
+      author: "John Doe",
+      date: "2021-01-01",
+      cover: "https://picsum.photos/200/300",
+      content: {},
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      difficulty: "easy",
+      author: "John Doe",
+      date: "2021-01-01",
+      cover: "https://picsum.photos/200/300",
+      content: {},
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      difficulty: "easy",
+      author: "John Doe",
+      date: "2021-01-01",
+      cover: "https://picsum.photos/200/300",
+      content: {},
+    },
+    {
+      id: 4,
+      title: "Lorem ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      difficulty: "easy",
+      author: "John Doe",
+      date: "2021-01-01",
+      cover: "https://picsum.photos/200/300",
+      content: {},
+    },
+  ];
+
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+    <>
+      <section>
+        <div className="container">
+          <div className="flex flex-col gap-y-3">
+            {Posts.map((post) => (
+              <div key={post.id} className="card bg-base-100 shadow-xl">
+                <figure>
+                  <img src={post.cover} alt="cover" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{post.title}</h2>
+                  <p>{post.description}</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Read</button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
 
