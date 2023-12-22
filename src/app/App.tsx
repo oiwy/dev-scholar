b27a2167ed3050fb1babef5c7b8c7ab1420c0e50
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-12-23 01:40:09 +0700

    feat: update App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,14 +4,17 @@ import { Home, NoMatch } from "@/pages";
 
 const App = () => {
   return (
-    <div className="flex h-screen flex-col justify-between gap-y-2">
+    <div className="flex h-screen flex-col justify-between">
       <Header />
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-      </main>
+      <div className="container flex gap-10 pb-6 pt-3">
+        <main className="w-full basis-10/12">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </main>
+        <aside className="h-[700px] basis-1/2 bg-black"></aside>
+      </div>
       <Footer />
     </div>
   );
